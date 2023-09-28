615b4eff7fe14bae46f40f5f782b8b3c280df76d
Tanjim <tanjim.rahat.dipto@gmail.com>
2023-09-28 23:10:22 +0600

    Added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#2a2a2a",
+        dark2: "#252525",
+        dark3: "#333333",
+
+        main: "#3177ff",
+      },
+    },
   },
   plugins: [],
 };
