fa45ec931d0c159e058cda5d3234ed8dc9ed751b
Tanjim <tanjim.rahat.dipto@gmail.com>
2023-09-29 12:09:08 +0600

    added column and pagination frame

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
 import Container from "./components/Container";
 import LogoText from "./components/LogoText";
 import Searchbar from "./components/Searchbar";
 import ExLinkIcon from "./components/icons/ExLink";
 import FilterIcon from "./components/icons/Filter";
+import { useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://kontests.net/api/v1/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
+
+  const formatDate = (data) => {
+    const date = new Date(data);
+    return Intl.DateTimeFormat("en-US", {
+      dateStyle: "medium",
+      timeStyle: "short",
+    }).format(date);
+  };
+
   return (
     <div className="fixed inset-0 bg-dark text-white">
       <header className="h-[10vh]">
@@ -20,8 +41,70 @@ function App() {
           </div>
         </Container>
       </header>
-      <main className="h-[80vh]"></main>
-      <footer className="h-[10vh] bg-red-500">
+
+      <main className="h-[80vh]">
+        <Container>
+          <section className="w-full h-[70vh]">
+            <div className="w-full h-[5vh] bg-black grid grid-cols-12 items-center font-medium text-sm translate-y-8">
+              <p className="text-center">SL</p>
+              <p className="text-center col-span-6">Name</p>
+              <p className="text-center col-span-3">Lifespan</p>
+              <p className="text-center col-span-2">Status</p>
+            </div>
+
+            <div className="h-[65vh] overflow-y-auto">
+              {data.map((datum, index) => (
+                <div
+                  key={datum.url}
+                  className={`w-full grid grid-cols-12 items-center py-6 ${
+                    index % 2 == 0 ? "bg-dark2" : "bg-dark3"
+                  }`}
+                >
+                  <p className="text-center font-medium">#0{index + 1}</p>
+
+                  <p className="col-span-6 flex flex-col gap-1">
+                    <a
+                      href={datum.url}
+                      target="_blank"
+                      className="hover:text-main"
+                    >
+                      {datum.name}
+                    </a>
+                    <a className="text-xs">({datum.site})</a>
+                  </p>
+
+                  <div className="col-span-3 text-sm flex flex-col">
+                    <p className="flex">
+                      <span className="w-10 font-medium text-main">Start:</span>{" "}
+                      {formatDate(datum.start_time)}
+                    </p>
+                    <p className="flex">
+                      <span className="w-10 font-medium text-main">End:</span>{" "}
+                      {formatDate(datum.end_time)}
+                    </p>
+                  </div>
+
+                  <p className="text-center col-span-2">{datum.status}</p>
+                </div>
+              ))}
+            </div>
+          </section>
+
+          <section className="h-[10vh] flex items-center justify-center gap-8">
+            <button className="flex items-center justify-center w-8 aspect-square rounded bg-main font-medium">
+              1
+            </button>
+            <button className="flex items-center justify-center w-8 aspect-square rounded font-medium">
+              2
+            </button>
+            <button className="flex items-center justify-center w-8 aspect-square rounded font-medium">
+              3
+            </button>
+          </section>
+        </Container>
+      </main>
+
+      <footer className="h-[10vh]">
         <Container className="h-full flex items-center justify-between">
           <div className="flex flex-col gap-1">
             <LogoText className="text-main" />
