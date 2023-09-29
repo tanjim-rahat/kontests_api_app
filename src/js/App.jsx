fe4745156ca40f9abe38336bb04cbda5b35f8316
Tanjim <tanjim.rahat.dipto@gmail.com>
2023-09-29 11:01:48 +0600

    added logo elements

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,7 +1,19 @@
+import Container from "./components/Container";
+
 function App() {
   return (
     <div className="fixed inset-0 bg-dark text-white">
-      <div></div>
+      <header className="h-[100px]">
+        <Container className="h-full flex items-center justify-between">
+          <a href="/">
+            <button aria-label="logo" role="hyperlink">
+              Kontests
+            </button>
+          </a>
+        </Container>
+      </header>
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
