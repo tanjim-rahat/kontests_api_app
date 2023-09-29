0387edb3be6adaa8f676cd1909b7908da900f986
Tanjim <tanjim.rahat.dipto@gmail.com>
2023-09-29 11:27:27 +0600

    added footer

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,11 +1,12 @@
 import Container from "./components/Container";
 import Searchbar from "./components/Searchbar";
+import ExLinkIcon from "./components/icons/ExLink";
 import FilterIcon from "./components/icons/Filter";
 
 function App() {
   return (
     <div className="fixed inset-0 bg-dark text-white">
-      <header className="h-[100px]">
+      <header className="h-[10vh]">
         <Container className="h-full flex items-center justify-between">
           <a href="/">
             <button
@@ -26,8 +27,39 @@ function App() {
           </div>
         </Container>
       </header>
-      <main></main>
-      <footer></footer>
+      <main className="h-[80vh]"></main>
+      <footer className="h-[10vh] bg-red-500">
+        <Container className="h-full flex items-center justify-between">
+          <div className="flex flex-col gap-1">
+            <a href="/">
+              <button
+                aria-label="logo"
+                role="hyperlink"
+                className="uppercase text-xl tracking-wide font-medium text-main"
+              >
+                Kontests
+              </button>
+            </a>
+
+            <p className="text-xs">
+              Created using{" "}
+              <a
+                href="https://kontests.net/api"
+                className="text-main underline"
+              >
+                kontest.net
+              </a>{" "}
+              free API
+            </p>
+          </div>
+
+          <a href="https://kontests.net/api">
+            <button>
+              <ExLinkIcon />
+            </button>
+          </a>
+        </Container>
+      </footer>
     </div>
   );
 }
